b13067c62f1e8821dad8abf3b738c798d45d786c
Ross Smith II <ross@smithii.com>
2023-11-30 23:13:01 -0800

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -584,5 +584,7 @@ def main() -> int:
 
 if __name__ == "__main__":
     sys.exit(main())
+
+# cspell:ignore pdfs
 # cSpell:ignore EXPANDEDBRANCHCODESID, expandedbranchcodesid, EXPANDEDBRANCHCODESIDA
 # cSpell:ignore MANYLAWSECTIONS, manylawsections, pdfkit, Pdfkit, prefs, pypdf
